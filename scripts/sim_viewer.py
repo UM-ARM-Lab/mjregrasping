fix external torque calculation. "sim_viewer" test script works!
implemented ReRun rendering of mujoco scene

scripts/sim_viewer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import logging
 from time import sleep
 
 import mujoco
@@ -7,57 +8,99 @@ import rerun as rr
 from mujoco import viewer
 
 from mjregrasping.buffer import Buffer
+from mjregrasping.rerun_visualizer import MjReRun
+
+logger = logging.getLogger(__name__)
+
+
+class SensorBuffer(Buffer):
+
+    def __init__(self, sensor_name, buffer_size):
+        super().__init__(buffer_size)
+        self.joint_name = sensor_name.replace("_torque", "")
+        self.sensor_name = sensor_name
 
 
 def main():
     np.set_printoptions(precision=3, suppress=True)
     rr.init('sim_viewer')
     rr.spawn()
-    model = mujoco.MjModel.from_xml_path('models/test_ext.xml')
-    data = mujoco.MjData(model)
+    xml_path = 'models/untangle_scene.xml'
+    m = mujoco.MjModel.from_xml_path(xml_path)
+    d = mujoco.MjData(m)
+    mujoco.mj_forward(m, d)
 
-    b1 = Buffer(10)
-    b2 = Buffer(10)
+    # iterate over all the sensors, check if they are torque sensors,
+    # and if so, add a buffer to store the torque values
+    buffers = []
+    torque_sensors = []
+    for i in range(m.nsensor):
+        sensor = m.sensor(i)
+        if sensor.type == mujoco.mjtSensor.mjSENS_TORQUE:
+            buffers.append(SensorBuffer(sensor.name, 10))
+            torque_sensors.append(sensor)
 
-    with mujoco.viewer.launch_passive(model, data) as viewer:
+    joint_to_qv_map = make_joint_qv_map(m)
+    mjrr = MjReRun(xml_path)
+
+    with mujoco.viewer.launch_passive(m, d) as viewer:
         while True:
-            rr.set_time_seconds('sim_time', data.time)
             viewer.sync()
 
-            torque1 = data.sensor('torque1').data
-            torque2 = data.sensor('torque2').data
-            torque1_dir = np.sign(np.dot(model.joint("joint1").axis, torque1))
-            torque2_dir = np.sign(np.dot(model.joint("joint2").axis, torque2))
-            torque1_mag_signed = torque1_dir * np.linalg.norm(torque1)
-            torque2_mag_signed = torque2_dir * np.linalg.norm(torque2)
-            external_torque1 = torque1_mag_signed - data.qfrc_bias[0]
-            external_torque2 = torque2_mag_signed - data.qfrc_bias[1]
+            mjrr.viz(m, d)
 
-            b1.insert(external_torque1)
-            b2.insert(external_torque2)
+            # iterate over each sensor and compute external torque
+            external_torques = []
+            for buffer in buffers:
+                sensor = m.sensor(buffer.sensor_name)
+                torque_rpy = d.sensor(sensor.name).data
+                # convention that torque sensors start with the name of the joint they are attached to
+                joint = m.joint(buffer.joint_name)
+                qv_idx = joint_to_qv_map[joint.id]
+                current_torque = joint.axis @ torque_rpy
+                buffer.insert(current_torque)
+                torque = np.mean(buffer.data)
+                external_torque = torque - d.qfrc_bias[qv_idx]
+                external_torques.append(external_torque)
+                rr.log_scalar(f'qfrc_bias/{joint.name}', d.qfrc_bias[qv_idx])
+                rr.log_scalar(f'external_torque/{joint.name}', external_torque)
 
-            external_torques = np.array([np.mean(b1.data), np.mean(b2.data)])
-            if (b1.full() and b2.full()) and np.any(np.abs(external_torques) > 0.5):
+            # FIXME: buffer is for the wrong variable
+            external_torques = np.array(external_torques)
+            if all([b.full() for b in buffers]) and np.any(np.abs(external_torques) > 3.0):
                 print("High torque!")
-                data.act = data.qpos + np.clip(data.act - data.qpos, -0.005, 0.05)
+                qpos_indices_for_sensor = np.array([m.joint(b.joint_name).qposadr[0] for b in buffers])
+                qpos_for_sensors = d.qpos[qpos_indices_for_sensor]
+                d.act = qpos_for_sensors + np.clip(d.act - qpos_for_sensors, -0.01, 0.01)
 
-            mujoco.mj_step(model, data, nstep=10)
-            sleep(0.01)
+            mujoco.mj_step(m, d, nstep=10)
+            sleep(0.05)
 
-            rr.log_scalar('scalars/torque/1', torque1_mag_signed)
-            rr.log_scalar('scalars/torque/2', torque2_mag_signed)
-            rr.log_scalar('scalars/qfrc_bias/1', data.qfrc_bias[0])
-            rr.log_scalar('scalars/qfrc_bias/2', data.qfrc_bias[1])
-            rr.log_scalar('scalars/external_torque/1', np.mean(b1.data))
-            rr.log_scalar('scalars/external_torque/2', np.mean(b2.data))
 
-            # override the controls set in the UI
-            rr.log_scalar(f'controls/qpos/1', data.qpos[0])
-            rr.log_scalar(f'controls/qpos/2', data.qpos[1])
-            rr.log_scalar(f'controls/v/1', data.ctrl[0])
-            rr.log_scalar(f'controls/v/2', data.ctrl[1])
-            rr.log_scalar(f'controls/act/1', data.act[0])
-            rr.log_scalar(f'controls/act/2', data.act[1])
+def make_joint_qv_map(model):
+    # Iterate ove all the joints and get their DOFs
+    # the total DOFs in all joints should equal model.nv
+    my_nv = 0
+    joint_to_qv_map = {}
+    for i in range(model.njnt):
+        joint = model.joint(i)
+        if joint.type == mujoco.mjtJoint.mjJNT_HINGE:
+            joint_to_qv_map[joint.id] = joint.qposadr[0]
+            joint_nv = 1
+        elif joint.type == mujoco.mjtJoint.mjJNT_SLIDE:
+            joint_nv = 1
+        elif joint.type == mujoco.mjtJoint.mjJNT_FREE:
+            joint_nv = 6
+        elif joint.type == mujoco.mjtJoint.mjJNT_BALL:
+            joint_nv = 3
+        else:
+            raise NotImplementedError('Unsupported joint type')
+
+        my_nv += joint_nv
+        logger.debug(f'Joint {joint.name} has {joint_nv} DOFs')
+        # only 1-dof hinge joints are supported
+    logger.debug(f'Total DOFs: {my_nv} vs {model.nv}')
+    return joint_to_qv_map
 
 
 if __name__ == '__main__':

src/mjregrasping/rerun_visualizer.py
@@ -1,13 +1,28 @@
-import mujoco
-import rerun as rr
+import logging
+from pathlib import Path
 
-from mujoco import mjtSensor
+import mujoco
+import numpy as np
+import rerun as rr
+from mujoco import mjtSensor, mjtGeom, mj_id2name
+from mujoco._structs import _MjDataGeomViews, _MjModelGeomViews
+from scipy.spatial.transform import Rotation
+from trimesh.creation import box, cylinder, capsule
+
+from mjregrasping.rviz import names, MujocoXmlMeshParser
+
+logger = logging.getLogger(f'rosout.{__name__}')
 
 
 class MjReRun:
 
-    def __init__(self):
-        pass
+    def __init__(self, xml_path):
+        self.mj_xml_parser = MujocoXmlMeshParser(xml_path)
+        rr.set_time_seconds('sim_time', 0.0)
+        rr.log_view_coordinates("world", up="+Z", timeless=True)
+        rr.log_arrow('world_x', [0, 0, 0], [1, 0, 0], color=(255, 0, 0), width_scale=0.02)
+        rr.log_arrow('world_y', [0, 0, 0], [0, 1, 0], color=(0, 255, 0), width_scale=0.02)
+        rr.log_arrow('world_z', [0, 0, 0], [0, 0, 1], color=(0, 0, 255), width_scale=0.02)
 
     def viz(self, m: mujoco.MjModel, d: mujoco.MjData):
         rr.set_time_seconds('sim_time', d.time)
@@ -23,3 +38,137 @@ class MjReRun:
                 rr.log_scalar(f'qpos/{joint.name}', float(d.qpos[joint.qposadr]))
 
         rr.log_scalar(f'contact/num_contacts', len(d.contact))
+
+        # self.viz_bodies(m, d)
+
+    def viz_bodies(self, m: mujoco.MjModel, d: mujoco.MjData):
+        for geom_id in range(m.ngeom):
+            geom = m.geom(geom_id)
+            geom_type = geom.type
+            geom_bodyid = geom.bodyid
+            body_name, parent_names = names(geom_bodyid, m)
+            entity_name = f"{parent_names[0]}/{body_name}"
+
+            if geom_type == mjtGeom.mjGEOM_BOX:
+                log_box(entity_name, m.geom(geom_id), d.geom(geom_id))
+            elif geom_type == mjtGeom.mjGEOM_CYLINDER:
+                log_cylinder(entity_name, m.geom(geom_id), d.geom(geom_id))
+            elif geom_type == mjtGeom.mjGEOM_CAPSULE:
+                log_capsule(entity_name, m.geom(geom_id), d.geom(geom_id))
+                pass
+            elif geom_type == mjtGeom.mjGEOM_SPHERE:
+                log_sphere(entity_name, m.geom(geom_id), d.geom(geom_id))
+            elif geom_type == mjtGeom.mjGEOM_MESH:
+                mesh_name = mj_id2name(m, mujoco.mjtObj.mjOBJ_MESH, geom.dataid)
+                # skip the model prefix, e.g. val/my_mesh
+                if '/' in mesh_name:
+                    mesh_name = mesh_name.split("/")[1]
+                mesh_file = Path(self.mj_xml_parser.get_mesh(mesh_name))
+                mesh_file = mesh_file.stem + ".glb"
+                if mesh_file is None:
+                    raise RuntimeError(f"Mesh {mesh_name} not found in XML file")
+                mesh_file = Path.home() / "mjregrasping_ws/src/mjregrasping/models/meshes" / mesh_file
+
+                if not mesh_file.exists():
+                    raise RuntimeError(f"Mesh {mesh_file} not found on disk")
+                # We use body pos/quat here under the assumption that in the XML, the <geom type="mesh" ... />
+                # has NO POS OR QUAT, but instead that info goes in the <body> tag
+                log_mesh(entity_name, m.body(geom_bodyid), d.body(geom_bodyid), mesh_file)
+            else:
+                logger.error(f"Unsupported geom type {geom_type}")
+                continue
+
+
+def name(*names):
+    """ joins names with slashes but ignores empty names """
+    return '/'.join([name for name in names if name])
+
+
+def get_transform(data: _MjDataGeomViews):
+    xmat = data.xmat.reshape(3, 3)
+    transform = np.eye(4)
+    transform[0:3, 0:3] = xmat
+    transform[0:3, 3] = data.xpos
+    return transform
+
+
+def log_plane(body_name, model: _MjModelGeomViews, data: _MjDataGeomViews):
+    transform = get_transform(data)
+    mesh = box(np.array([model.size[0], model.size[1], 0.001]), transform)
+    rr.log_mesh(entity_path=name(body_name, model.name),
+                positions=mesh.vertices,
+                indices=mesh.faces,
+                albedo_factor=model.rgba)
+
+
+def log_capsule(body_name, model: _MjModelGeomViews, data: _MjDataGeomViews):
+    transform = get_transform(data)
+    mesh = capsule(radius=model.size[0], height=2 * model.size[1], transform=transform, count=[6, 6])
+    rr.log_mesh(entity_path=name(body_name, model.name),
+                positions=mesh.vertices,
+                indices=mesh.faces,
+                albedo_factor=model.rgba)
+
+def log_cylinder(body_name, model: _MjModelGeomViews, data: _MjDataGeomViews):
+    transform = get_transform(data)
+    mesh = cylinder(radius=model.size[0], height=2 * model.size[1], transform=transform, sections=16)
+    rr.log_mesh(entity_path=name(body_name, model.name),
+                positions=mesh.vertices,
+                indices=mesh.faces,
+                albedo_factor=model.rgba)
+
+
+def log_sphere(body_name, model: _MjModelGeomViews, data: _MjDataGeomViews):
+    rr.log_point(entity_path=name(body_name, model.name),
+                 position=data.xpos,
+                 radius=model.size[0],
+                 color=tuple(model.rgba))
+
+
+def log_mesh(body_name, model, data, mesh_file):
+    transform = get_transform(data)
+    with open(mesh_file, 'rb') as f:
+        contents = f.read()
+    rr.log_mesh_file(entity_path=name(body_name, model.name),
+                     mesh_format=rr.MeshFormat.GLB,
+                     mesh_file=contents,
+                     transform=transform[:3, :])
+
+
+def log_box(body_name, model: _MjModelGeomViews, data: _MjDataGeomViews):
+    transform = get_transform(data)
+    mesh = box(model.size * 2, transform)
+    rr.log_mesh(entity_path=name(body_name, model.name),
+                positions=mesh.vertices,
+                indices=mesh.faces,
+                albedo_factor=model.rgba)
+
+
+def log_rotational_velocity(entity_name,
+                            position,
+                            rotational_velocity,
+                            color,
+                            stroke_width,
+                            max_vel=1.5,
+                            z=0.12,
+                            radius=0.1):
+    """
+    Draw an arc with an arrow tip centered a position, and with a radius and length proportional to the rotational velocity.
+    """
+    vel_rel = rotational_velocity / max_vel
+    angles = np.linspace(0, 2 * np.pi * vel_rel, 16)
+    arc_xs = position[0] + np.cos(angles) * radius
+    arc_ys = position[1] + np.sin(angles) * radius
+    arc_positions = np.stack([arc_xs, arc_ys, np.ones_like(arc_xs) * z], axis=1)
+    # main body of the arrow
+    rr.log_line_strip(entity_name + '/arc', arc_positions, color=color, stroke_width=stroke_width)
+    # arrow tips
+    tip_positions = [
+        position + (arc_positions[-6] - position) * 0.8,
+        position + (arc_positions[-6] - position) * 1.2,
+    ]
+    tip_positions[0][2] = z
+    tip_positions[1][2] = z
+    rr.log_line_segments(entity_name + '/tip',
+                         [arc_positions[-1], tip_positions[0], arc_positions[-1], tip_positions[1]], color=color,
+                         stroke_width=stroke_width)